37ec003847a4092d639513a59d730955232888e4
⚡ bzt 么 lezin™ ⚡ <106700535+txylezin@users.noreply.github.com>
2023-09-26 13:22:07 -0300

    Create questions.js

---
+++ js/questions.js
@@ -0,0 +1,11 @@
+// creating  an array and pass the number, questions, options, and answers  
+let questions = [
+  {
+    numb: 1,
+    questions: "Quem foi a única pessoa na história a receber o Prêmio Nobel em áreas científicas diferentes?",
+    answer: "Marie Curiee",
+    options: [
+      "Albert Einstein",
+      "Marie Curiee",
+      "Stephen Hawking",
+      "Línus Pauling",
