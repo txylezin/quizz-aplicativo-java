343f4b06086d18f77c9d014c626c4647d5095d77
⚡ bzt 么 lezin™ ⚡ <106700535+txylezin@users.noreply.github.com>
2023-10-23 14:04:31 -0300

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -9,3 +9,64 @@ let questions = [
       "Marie Curiee",
       "Stephen Hawking",
       "Línus Pauling",
+    ]
+  },
+  {
+    numb: 2,
+    question: "Qual a seleção que ganhou mais copas ?",
+    answer: "Brasil",
+    options: [
+      "Alemanha",
+      "Brasil",
+      "Italia",
+      "Chile"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Qual o nome do rei do futebol Pelé?",
+    answer: "Edson Arantes do Nascimento",
+    options: [
+      "Francisco Silva",
+      "Edson Arantes do Nascimento",
+      "Milton Nascimento",
+      "Galvão Bueno"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Quantos Paulistas tem o Corinthians?",
+    answer: "30",
+    options: [
+      "12",
+      "21",
+      "30",
+      "33"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Qual o maior ídolo da história do Corinthians?",
+    answer: "Cassio Ramos",
+    options: [
+      "Neto",
+      "Danilo",
+      "Cassio Ramos",
+      "Roger Guedes"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
